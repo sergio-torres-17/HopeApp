8a7d009c976630bc98f09693235d2afa1ca46ea0
sergio-torres-17 <50086305+sergio-torres-17@users.noreply.github.com>
2022-04-09 00:08:29 -0500

    Actualización de paquetes del Building

--- app/src/main/java/com/MultiDev/hopeapp/Login.java
+++ app/src/main/java/com/MultiDev/hopeapp/Login.java
@@ -3,12 +3,40 @@ package com.MultiDev.hopeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class Login extends AppCompatActivity {
-
+    private EditText txtUser, txtPass;
+    private ImageView imgLogo;
+    private Button btnLogIn;
+    private Animation anim1, anim2;
+    private CheckBox chkPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        this.inicializarValores();
+        this.animar();
+
+    }
+    private void inicializarValores(){
+        this.txtUser = findViewById(R.id.edtUsuario);
+        this.txtPass = findViewById(R.id.edtPassword);
+        this.btnLogIn= findViewById(R.id.btnLogin);
+        this.imgLogo = findViewById(R.id.imgLogoLogin);
+        this.chkPass = findViewById(R.id.chkShowPass);
+    }
+    private void animar(){
+        anim1 = AnimationUtils.loadAnimation(Login.this, R.anim.down_des);
+        this.txtUser.setAnimation(this.anim1);
+        this.txtPass.setAnimation(this.anim1);
+        this.btnLogIn.setAnimation(this.anim1);
+        this.imgLogo.setAnimation(this.anim1);
+        this.chkPass.setAnimation(this.anim1);
     }
 }
---
+++ app/src/main/java/com/MultiDev/hopeapp/WebService/ConstantesURL.java
@@ -0,0 +1,8 @@
+package com.MultiDev.hopeapp.WebService;
+
+public class ConstantesURL {
+    //Direccion Backend
+    private final String URL_WS = "http://192.168.152.233/hopeappbackend/Request/";
+    //Peticiones doctores
+    private final String R_LOGIN = "LoginDoctor.php";
+}
